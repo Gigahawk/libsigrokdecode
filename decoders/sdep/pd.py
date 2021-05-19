8b5afed6bba2a0d1ad0b9e06216dcc27392e452a
Jasper Chan <jasperchan515@gmail.com>
2021-05-19 02:58:29 -0700

    Support decoding MOSI packets

--- decoders/sdep/pd.py
+++ decoders/sdep/pd.py
@@ -19,6 +19,7 @@
 ##
 
 import sigrokdecode as srd
+from binascii import hexlify
 
 
 class Decoder(srd.Decoder):
@@ -32,10 +33,13 @@ class Decoder(srd.Decoder):
     outputs = []
     tags = ['Adafruit']
     annotations = (
-        ('packet_type', 'Packet type'),
+        ('msg_type', 'Message Type'),
+        ('cmd_id', 'Command ID'),
+        ('payload_len', 'Payload Length'),
+        ('payload', 'Payload'),
     )
     annotation_rows = (
-        ('data', 'Data', (0,)),
+        ('mosi_packets', 'MOSI Packets', (0, 1, 2, 3,)),
     )
 
     def __init__(self):
@@ -43,6 +47,8 @@ class Decoder(srd.Decoder):
 
     def reset(self):
         self.mosi_state = 'START'
+        self.mosi_command_id = [None, None]
+        self.mosi_command_id_ss = None
 
     def start(self):
         self.out_ann = self.register(srd.OUTPUT_ANN)
@@ -53,4 +59,38 @@ class Decoder(srd.Decoder):
 
     def decode(self, ss, es, data):
         ptype, mosi, miso = data
-        self.put(ss, es, self.out_ann, [0, ['AAAA', 'AA']])
+        if ptype != 'DATA':
+            return
+
+        if self.mosi_state == 'START' and mosi == 0x10:
+            self.put(ss, es, self.out_ann, [0, ['Command', 'cmd']])
+            self.mosi_state = 'COMMAND_ID1'
+            return
+
+        if self.mosi_state == 'COMMAND_ID1':
+            self.mosi_command_id[0] = mosi
+            self.mosi_state = 'COMMAND_ID2'
+            self.mosi_command_id_ss = ss
+            return
+
+        if self.mosi_state == 'COMMAND_ID2':
+            self.mosi_command_id[1] = mosi
+            cmd_id = hexlify(bytes(reversed(self.mosi_command_id))).decode()
+            self.put(self.mosi_command_id_ss, es, self.out_ann, [1, ['Command ID: 0x{}'.format(cmd_id), 'cmd_id: {}'.format(cmd_id)]])
+            self.mosi_state = 'PAYLOAD_LEN'
+            return
+
+        if self.mosi_state == 'PAYLOAD_LEN':
+            more = int(bool(mosi & 0x80))
+            payload_len = mosi & 0b11111
+            self.put(ss, es, self.out_ann, [2, ['Payload Length: {}, More: {}'.format(payload_len, more), 'len: {}, more: {}'.format(payload_len, more)]])
+            self.mosi_state = 'PAYLOAD'
+            self.payload_len = payload_len
+            return
+
+        if self.mosi_state == 'PAYLOAD':
+            self.payload_len -= 1
+            self.put(ss, es, self.out_ann, [3, [chr(mosi)]])
+            if self.payload_len == 0:
+                self.mosi_state = 'START'
+            return
